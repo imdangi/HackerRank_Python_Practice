ffc89b4e3c29e2a9a95c3bc91af7023e319a65ad
Manoj Kumar <82015676+imdangi@users.noreply.github.com>
2021-07-05 07:40:53 +0530

    updated

--- Que17_input.py
+++ Que17_input.py
@@ -1,14 +1,39 @@
 def polCheck(x,k,p):
-    print(p)
-
-
-    sum=0
-    
-       
-    if(sum==k):
+    res=[]
+    sum=[]
+    #removing ** from the list
+    for ele in p:
+        temp=ele.split("*")
+        res.append(temp)
+    print(res)
+    #storing elements after solving the operands
+    for i in res:
+        if i[0]=="x" and len(i)==2:
+            temp=x**int(i[1])
+            sum.append(temp)
+        elif i[0]=="x" and len(i)==1:
+            temp=x**1
+            sum.append(temp)
+        elif i[0]=='+' or i[0]=='-':
+            sum.append(i[0])
+        else:
+            sum.append(int(i[0]))
+    ans=0
+    for i in range(len(sum)):
+        if i==0:
+            ans+=sum[i]
+        else:
+            if sum[i]=="+":
+                ans+=sum[i+1]
+                i+=1
+            elif sum[i]=="-":
+                ans-=sum[i+1]
+                i+=1
+    if(ans==k):
         return True
     else:
         return False
+
 x,k=input().strip().split()
 p=input().strip().split()
 print(polCheck(int(x),int(k),p))
--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 print("Welcome all !")
 
-try:
-    print(1/0)
-except Exception as e:
-    print("Error Code:",e)
+def polCheck(x,k):
+    sum=0
+    for i in range(k-1,-1,-1):
+        sum+=x**i
+    if(sum==k):
+        return True
+    else:
+        return False
+x,k=input().strip().split()
+
+print(polCheck(int(x),int(k)))
